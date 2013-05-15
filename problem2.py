96d52e03dc9e7a5f6eae721a5c838ef43bc2aea2
John Sandall <john.sandall@gmail.om>
2013-05-15 10:13:22 +0100

    Added problem2.py

---
+++ problem2.py
@@ -0,0 +1,14 @@
+n = 4000000
+t_1 = 1
+t_2 = 2
+total = t_2
+
+while t_1 + t_2 < n:
+    t = t_1 + t_2
+    t_1 = t_2
+    t_2 = t
+    total += t if (t % 2 == 0) else 0
+        
+print total
+    
+
