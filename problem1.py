1e44cc1cc887bde3fdd68af42d1c096e8bd8e04c
John Sandall <john.sandall@gmail.om>
2013-05-15 09:57:37 +0100

    Forgot to remove multiples of 15

--- problem1.py
+++ problem1.py
@@ -1,12 +1,8 @@
-n = 1000
-total = 0
-
-def addMultiples(x,multiplier):
+def sumMultiples(multiplier,n):
+    x = 0
     for i in range( 1 , ((n-1) / multiplier)+1 ):
         x = x + multiplier*i
     return x
     
-total = addMultiples(total,3)
-total = addMultiples(total,5)
-
+total = sumMultiples(3,1000) + sumMultiples(5,1000) - sumMultiples(15,1000)
 print total
