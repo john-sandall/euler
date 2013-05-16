4ff9b99f5c70c886ecf9eebcd013abd26951c356
John Sandall <john.sandall@gmail.om>
2013-05-16 13:25:08 +0100

    Made problem1.py PEP-8 compliant

--- problem1.py
+++ problem1.py
@@ -1,8 +1,16 @@
-def sumMultiples(multiplier,n):
+# Project Euler Problem #1: http://projecteuler.net/problem=1
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these
+# multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+
+# sumMultiples() returns the sum of all the multiples of a number less than n. We then add these up for 3 & 5 and
+# remove double counting.
+
+
+def sum_multiples(multiplier, n):
     x = 0
-    for i in range( 1 , ((n-1) / multiplier)+1 ):
-        x = x + multiplier*i
+    for i in range(1, ((n-1) / multiplier) + 1):
+        x = x + multiplier * i
     return x
-    
-total = sumMultiples(3,1000) + sumMultiples(5,1000) - sumMultiples(15,1000)
+
+total = sum_multiples(3, 1000) + sum_multiples(5, 1000) - sum_multiples(15, 1000)
 print total
